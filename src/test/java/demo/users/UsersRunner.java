e1522f9b1690d1158028954ac50d76ba70d66f6a
Admin <chaulagain.amit@rew3.com>
2018-09-20 17:36:03 +0545

    ready for testing graphQL

--- src/test/java/demo/users/UsersRunner.java
+++ src/test/java/demo/users/UsersRunner.java
@@ -4,7 +4,7 @@ import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
-@CucumberOptions(features = "classpath:demo/users/transactions.feature")
+@CucumberOptions(features = "classpath:demo/users/users.feature")
 public class UsersRunner {
 
 }
